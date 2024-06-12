992a9f761e1a9dcac36e11b4d363bda28b57ba38
gemperador <gabriemperador@gmail.com>
2024-06-11 21:10:17 -0700

    modified pages folder

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,15 +1,19 @@
+// src/App.jsx
+// Create an Apollo Provider to make every request work with the Apollo server.
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
+import { ApolloProvider } from '@apollo/client';
+import client from './utils/apolloClient';
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
 export default App;
+
